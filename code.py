6a3ebaa160439cb1d3868843d73b8c56d0ad97c0
nilton <nilton.velez@udea.edu.co>
2017-09-03 20:48:51 -0500

    template changed

--- code.py
+++ code.py
@@ -19,7 +19,7 @@ def calcular():
 		print(n, xk, minn, maxx)
 		prg3 = Programa3()
 		prg3.CalcularPrediccion(xk, minn, maxx, n)	
-		return render_template("template.html", x=prg3.x, y=prg3.y, xcuadrada=prg3.x2, ycuadrada=prg3.y2, xy=prg3.xy, prediccion=prg3.yk, xmedia=prg3.xMedia, ymedia=prg3.yMedia, sumx2=prg3.sumXiCuadrada, sumy2=prg3.sumYiCuadrada, sumxy=prg3.sumXiYi, b0=prg3.B0, b1=prg3.B1)
+		return render_template("template.html", x=prg3.x, y=prg3.y, xcuadrada=prg3.x2, ycuadrada=prg3.y2, xy=prg3.xy, prediccion=prg3.yk, xmedia=prg3.xMedia, ymedia=prg3.yMedia, sumx2=prg3.sumXiCuadrada, sumy2=prg3.sumYiCuadrada, sumxy=prg3.sumXiYi, b0=prg3.B0, b1=prg3.B1, n=n)
 
 if __name__ == '__main__':
 	app.run()
