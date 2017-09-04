fac5ddb8e7ecf3b2261ca1549b3de1a9caabf6c8
nilton <nilton.velez@udea.edu.co>
2017-09-03 20:53:35 -0500

    template changed

--- code.py
+++ code.py
@@ -16,10 +16,13 @@ def calcular():
 		xk = request.form['xk']
 		minn = request.form['min']
 		maxx = request.form['max']
+		numeros=[]
+		for i in range(0,n):
+			numeros.append(i)
 		print(n, xk, minn, maxx)
 		prg3 = Programa3()
 		prg3.CalcularPrediccion(xk, minn, maxx, n)	
-		return render_template("template.html", x=prg3.x, y=prg3.y, xcuadrada=prg3.x2, ycuadrada=prg3.y2, xy=prg3.xy, prediccion=prg3.yk, xmedia=prg3.xMedia, ymedia=prg3.yMedia, sumx2=prg3.sumXiCuadrada, sumy2=prg3.sumYiCuadrada, sumxy=prg3.sumXiYi, b0=prg3.B0, b1=prg3.B1, n=n)
+		return render_template("template.html", x=prg3.x, y=prg3.y, xcuadrada=prg3.x2, ycuadrada=prg3.y2, xy=prg3.xy, prediccion=prg3.yk, xmedia=prg3.xMedia, ymedia=prg3.yMedia, sumx2=prg3.sumXiCuadrada, sumy2=prg3.sumYiCuadrada, sumxy=prg3.sumXiYi, b0=prg3.B0, b1=prg3.B1, numeros=numeros)
 
 if __name__ == '__main__':
 	app.run()
