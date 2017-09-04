8e85a7d4d109fb0149783b67eff49a3d771545f6
nilton <nilton.velez@udea.edu.co>
2017-09-03 21:13:51 -0500

    calculos realizados

--- code.py
+++ code.py
@@ -19,10 +19,9 @@ def calcular():
 		numeros=[]
 		for i in range(0,int(n)):
 			numeros.append(i)
-		print(n, xk, minn, maxx)
 		prg3 = Programa3()
 		prg3.CalcularPrediccion(xk, minn, maxx, n)	
-		return render_template("template.html", x=prg3.x, y=prg3.y, xcuadrada=prg3.x2, ycuadrada=prg3.y2, xy=prg3.xy, prediccion=prg3.yk, xmedia=prg3.xMedia, ymedia=prg3.yMedia, sumx2=prg3.sumXiCuadrada, sumy2=prg3.sumYiCuadrada, sumxy=prg3.sumXiYi, b0=prg3.B0, b1=prg3.B1, numeros=numeros)
+		return render_template("template.html",r=prg3.r, xk=xk, x=prg3.x, y=prg3.y, xcuadrada=prg3.x2, ycuadrada=prg3.y2, xy=prg3.xy, prediccion=prg3.yk, xmedia=prg3.xMedia, ymedia=prg3.yMedia, sumx2=prg3.sumXiCuadrada, sumy2=prg3.sumYiCuadrada, sumxy=prg3.sumXiYi, b0=prg3.B0, b1=prg3.B1, numeros=numeros, sumXi=prg3.sumXi, sumYi=prg3.sumYi)
 
 if __name__ == '__main__':
 	app.run()
